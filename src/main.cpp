cf8a247792ef0de8f77e9172ccb1907ee9f2d738
WuXiao <wuxiao20020211@163.com>
2021-03-24 23:13:05 +0800

    Update main.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
 
 	cout << "Eight_Eggs says:\"Hello,GitHub!\""<<endl;  // hello from Eight_Eggs
+	cout << "WuXiao says:\"Hello, GitHub!\"" << endl;   // hello from Wu Xiao
 
 	system("pause");
 	return 0;
